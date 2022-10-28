f536b3ef4d32e97a240b96994c818461339bc643
Noman Ejaz <nomanejaz132@gmail.com>
2022-10-28 16:04:46 +0500

    minor changes

--- src/components/LinkButton.js
+++ src/components/LinkButton.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const LinkButton = ({ label, id, link }) => {
+const LinkButton = ({ label, id, link, title }) => {
   return (
     <a
       href={link}
       id={id}
+      title={title}
       className="py-6 w-full text-[#101828] text-lg text-center font-medium font-inter bg-[#EAECF0] hover:bg-[#D0D5DD] rounded-lg border-[#EAECF0] hover:border-[#D0D5DD] drop-shadow-[0_1px_2px_rgba(16,24,40,0.05)] cursor-pointer"
     >
       {label}
