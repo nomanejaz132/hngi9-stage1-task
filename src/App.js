d1acd954a4b51a02a5577abca8e99066989d52d4
Noman Ejaz <nomanejaz132@gmail.com>
2022-10-28 09:49:22 +0500

    layout

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import ProfileAvatar from "./components/ProfileImage";
 
 function App() {
   return (
-    <div className="mx-10">
-      <button className="group cursor-pointer focus:ring focus:ring-[#F2F4F7] focus:rounded-full">
-        <ShareIcon />
-      </button>
-      <ProfileAvatar />
-    </div>
+    <main className="max-w-6xl mx-auto pt-16 pb-12">
+      <div className="relative flex justify-center">
+        <button className="absolute top-[-21px] right-[284px] group cursor-pointer focus:ring focus:ring-[#F2F4F7] focus:rounded-full">
+          <ShareIcon />
+        </button>
+        <ProfileAvatar />
+      </div>
+    </main>
   );
 }
 
